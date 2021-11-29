082bbcd2b18b7f121de7bce76beebf7bd0d730f1
Henrike S <roomwoud@users.noreply.github.com>
2021-11-29 10:21:02 +0100

    lab

---
+++ main_app.py
@@ -0,0 +1,11 @@
+from test_python_modules import df_form  
+ 
+def main():
+   
+    df_form()
+
+
+if __name__ == "__main_app__":
+    main()
+
+main()
---
+++ test_python_modules.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import math
+
+def df_form():
+    df = pd.DataFrame([[1,2,3],[4,5,6]])
+    print(df.info())
+    return df
+
+
